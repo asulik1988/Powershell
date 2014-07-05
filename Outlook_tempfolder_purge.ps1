8d4a67128db8f7f0ba1492a31d4c27bf0c335e01
asulik1988 <adamsulik1988@gmail.com>
2014-07-05 09:30:33 -0400

    Create Outlook_tempfolder_purge.ps1
    Purges temp folder in Outlook 2010. When the temp folder get's large, Outlook will error out when users try to preview email attachments, especially tiff and pdf files as they can be larger in size.

---
+++ Outlook_tempfolder_purge.ps1
@@ -0,0 +1,5 @@
+$a = Get-ItemProperty -Path HKCU:\SOFTWARE\MICROSOFT\OFFICE\14.0\OUTLOOK\SECURITY -Name OutlookSecureTempFolder
+$a -match "@{OutlookSecureTempFolder=(?<content>.*)}"
+$b = $matches['content']
+$b = $b + "\*"
+remove-item $b -Recurse -force
